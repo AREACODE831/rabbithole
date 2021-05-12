2aeed7adb4f786b1cc3b8018a26bd75e14f06bf8
soovinchoi <soovinnnnnn@gmail.com>
2021-05-13 06:41:51 +0900

    adding the level 2

--- sketch.js
+++ sketch.js
@@ -8,6 +8,7 @@ let lives = 1;
 let w = 600;
 let h = 600;
 let player;
+let alice;
 let coins = [];
 let enemies = [];
 let fails = [];
@@ -21,6 +22,9 @@ let failImg;
 let safeImg;
 let gameBgc;
 let introImg;
+let holeImg;
+let eyeImg;
+let inImg;
 
 
 
@@ -33,6 +37,10 @@ function preload() {
   safeImg = loadImage('asset/cha.png');
   introImg = loadImage('asset/intro.png');
   //bgImg = loadImage('asset/bg.png');
+  holeImg = loadImage('asset/2.png');
+  eyeImg = loadImage('asset/eye.jpg');
+  inImg = loadImage('asset/ra.png');
+
 }
 
 
@@ -47,6 +55,7 @@ function setup() {
   textFont('Futura');
 
   player = new Player();
+  alice = new Alice();
 
   //coins[0] = new Coin();
   coins.push(new Coin());
@@ -64,13 +73,26 @@ function draw() {
       title();
       cnv.mouseClicked(titleMouseClicked);
       break;
+    case 'instruction':
+      instruction();
+      cnv.mouseClicked(instructMouseClicked);
+      break;
+
     case 'level 1':
       level1();
       cnv.mouseClicked(level1MouseClicked);
       break;
+    case 'level 2':
+      level2();
+      cnv.mouseClicked(level2MouseClicked);
+      break;
     case 'you win':
       youWin();
       cnv.mouseClicked(youWinMouseClicked);
+      break;
+    case 'final':
+      final();
+      cnv.mouseClicked(finalMouseClicked);
       break;
     case 'game over':
       gameOver();
@@ -95,16 +117,33 @@ function draw() {
 function keyPressed() {
   if (keyCode == LEFT_ARROW) {
     player.direction = 'left'
+    alice.direction = 'left'
   } else if (keyCode == RIGHT_ARROW) {
     player.direction = 'right'
+    alice.direction = 'right'
   } else if (keyCode == UP_ARROW) {
     player.direction = 'up'
+    alice.direction = 'up'
   } else if (keyCode == DOWN_ARROW) {
     player.direction = 'down'
+    alice.direction = 'down'
   } else if (key == ' ') {
     player.direction = 'still';
+    alice.direction = 'still';
   }
+
+
+  // if (keyCode == 'a') {
+  //   alice.direction = 'left'
+  // } else if (keyCode == 'd') {
+  //   alice.direction = 'right'
+  // } else if (keyCode == 'w') {
+  //   alice.direction = 'up'
+  // } else if (keyCode == 's') {
+  //   alice.direction = 'down'
+  // }
 }
+
 
 
 function keyReleased() {
@@ -127,6 +166,9 @@ function keyReleased() {
   if (numberKeysPressed == 0) {
     player.direction = 'still';
   }
+  if (numberKeysPressed == 0) {
+    alice.direction = 'still';
+  }
 }
 
 function title() {
@@ -137,15 +179,43 @@ function title() {
   textAlign(CENTER);
   //text('Rabbit Hole', w / 2, h / 5);
   image(titleImg, w / 2, h / 3, 300, 250);
-  image(introImg, w / 4, h / 1.2, 300, 250);
-
+  image(inImg, w / 2, h / 1.25, 300, 250);
+  push();
   textSize(30);
-  //text('click anywhere to start!', w / 2, h / 1.05);
+  textFont('VT323');
+  text('click anywhere to start!', w / 2, h / 1.5);
+  pop();
 }
 
 function titleMouseClicked() {
   console.log('canvas is clicked on title page');
-  state = 'level 1'
+  state = 'instruction'
+}
+
+function instruction(){
+  background(147, 103, 255);
+  textSize(30);
+  textFont('VT323');
+  text('Rabbit Hole', w / 2, h / 5);
+  push();
+  textSize(22);
+  textAlign(LEFT);
+  text('Are you are ready to fall into the rabbit hole of adventure?', w / 24, h / 1.8);
+
+  text('You can use arrows to move around to get or lose the points', w / 24, h / 2.35);
+  text('by facing the obstacles coming down.', w / 24, h / 2.15);
+
+  text('Nobody but curious person can see the White Rabbit.', w / 24, h / 3);
+  // text('by facing the obstacles coming down.', w / 20, h / 1.9);
+
+
+  pop();
+  image(introImg, w / 5.3, h / 1.1, 225, 225);
+
+}
+
+function instructMouseClicked(){
+  state = 'level 1';
 }
 
 function level1() {
@@ -293,23 +363,40 @@ function level1MouseClicked() {
 
 
 }
+function level2(){
+   background(107, 242, 217);
+   image(eyeImg, w / 2, h / 1, 700, 700);
+   image(holeImg, w / 2, h / 1.19, 500, 700);
+   textSize(20);
+   text('still working on this part!',  w / 3, h / 30);
+   text('move(arrow)+click anywhere to end this',  w / 2, h / 15);
+
+   alice.display();
+   alice.move();
+
+
+}
+
+function level2MouseClicked(){
+state = 'final';
+}
 
 function youWin() {
   background(255, 126, 177);
   textSize(50);
   textFont('Girassol');
   stroke(255);
-  text('Hello, Alice.', w / 2, h / 2);
+  text('Hello, Alice-', w / 2, h / 2);
   textSize(30);
   text('where have you been?', w / 2, h * 3 / 5);
-  textSize(15);
-  text('click anywhere to restart!', w / 2, h * 3 / 4);
+  textSize(19);
+  text('click anywhere to resume', w / 2, h * 3 / 4);
 
 }
 
 function youWinMouseClicked() {
-  state = 'title';
-  points = 1;
+  state = 'level 2';
+  //points = 1;
 }
 
 function gameOver() {
@@ -336,6 +423,23 @@ function gameOver() {
   }
 
 }
+
+function final(){
+  background(147, 103, 255);
+  push();
+  textAlign(LEFT);
+  textSize(20);
+  text('“This is impossible!”', w / 6, h / 5);
+  textSize(25);
+  text('“Only if you believe it is.”', w / 6, h / 4);
+  text('click anywhere to go back to wake up', w / 6, h / 1.09);
+  pop();
+}
+
+function finalMouseClicked(){
+  state = 'title';
+}
+
 function gameBg() {
   push()
   background(gameBgc);
